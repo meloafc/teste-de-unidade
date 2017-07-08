687bb9125e2caeafb660dd1d779dcf27fd393d84
Alex Fabiano <afabiano15@gmail.com>
2017-07-08 19:51:03 -0400

    aula 1

---
+++ src/main/java/leilao/teste/TesteDoAvaliador.java
@@ -0,0 +1,37 @@
+package leilao.teste;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import leilao.dominio.Lance;
+import leilao.dominio.Leilao;
+import leilao.dominio.Usuario;
+import leilao.servico.Avaliador;
+
+public class TesteDoAvaliador {
+
+	@Test
+	public void deveEntenderLancesEmOrdemCrescente() {		
+		// parte 1: cenario
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("José");
+		Usuario maria = new Usuario("Maria");
+		
+		Leilao leilao = new Leilao("Playstation 3 Novo");
+		
+		leilao.propoe(new Lance(joao, 250.0));
+		leilao.propoe(new Lance(jose, 300.0));
+		leilao.propoe(new Lance(maria, 400.0));
+		
+		// parte 2: acao
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		// parte 3: validacao
+		double maiorEsperado = 400;
+		double menorEsperado = 250;
+		
+		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+	}
+}
